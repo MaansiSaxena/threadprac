09584e5d6c9cfef5fb6ace4c1175b16956d4eec3
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-09-14 18:06:31 +0530

    first commit

---
+++ sleeplocktimer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace sleeplocktimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Thread paused for {0} second", 1);
+                // Pause thread for 1 second
+                Thread.Sleep(1000);
+                Console.WriteLine("i value: {0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
